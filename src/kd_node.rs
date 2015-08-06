7d407dde5374ba21966322f567b49ce0141933d0
mrhooray <ruihu.pvt@gmail.com>
2015-08-06 02:13:09 -0700

    0.0.1

---
+++ src/kd_node.rs
@@ -0,0 +1,227 @@
+use std;
+use std::collections::BinaryHeap;
+use ::heap_element::HeapElement;
+use ::util;
+
+pub struct KdNode<'a, T: 'a> {
+    // node
+    left: Option<*mut KdNode<'a, T>>,
+    right: Option<*mut KdNode<'a, T>>,
+    // common
+    dimensions: usize,
+    capacity: usize,
+    size: usize,
+    min_bounds: Box<[f64]>,
+    max_bounds: Box<[f64]>,
+    // stem
+    split_value: Option<f64>,
+    split_dimension: Option<usize>,
+    // leaf
+    points: Option<Vec<&'a [f64]>>,
+    bucket: Option<Vec<&'a T>>,
+}
+
+pub enum ErrorKind {
+    WrongDimension,
+    NonFiniteCoordinate
+}
+
+impl<'a, T: 'a> KdNode<'a, T> {
+    pub fn new(dims: usize) -> KdNode<'a, T> {
+        KdNode::new_with_capacity(dims, 2^4)
+    }
+
+    pub fn new_with_capacity(dimensions: usize, capacity: usize) -> KdNode<'a, T> {
+        let min_bounds = vec![std::f64::NAN; dimensions];
+        let max_bounds = vec![std::f64::NAN; dimensions];
+        KdNode {
+            left: None,
+            right : None,
+            dimensions: dimensions,
+            capacity: capacity,
+            size: 0,
+            min_bounds: min_bounds.into_boxed_slice(),
+            max_bounds: max_bounds.into_boxed_slice(),
+            split_value : None,
+            split_dimension : None,
+            points: Some(vec![]),
+            bucket: Some(vec![]),
+        }
+    }
+
+    pub fn nearest<F> (&self, point: &[f64], num: usize, distance: &F) -> Result<Vec<(f64, &T)>, ErrorKind>
+        where F: Fn(&[f64], &[f64]) -> f64 {
+            match self.check_point(point) {
+                Err(err) => return Err(err),
+                Ok(_) => {}
+            };
+            let pending = &mut BinaryHeap::<HeapElement<&KdNode<T>>>::new();
+            let mut evaluated = BinaryHeap::<HeapElement<&T>>::new();
+            let num = std::cmp::min(num, self.size);
+            pending.push(HeapElement {
+                distance: 0f64,
+                element: self
+            });
+            while pending.len() > 0 &&
+                (evaluated.len() < num ||
+                 ((pending.peek().unwrap().distance * -1f64) < evaluated.peek().unwrap().distance)) {
+                self.nearest_step(point, num, distance, pending, &mut evaluated);
+            }
+            let mut result = vec![];
+            loop {
+                match evaluated.pop() {
+                    None => {
+                        result.reverse();
+                        return Ok(result);
+                    },
+                    Some(e) => result.push((e.distance, e.element)),
+                }
+            }
+        }
+
+    fn nearest_step<F> (&self, point: &[f64], num: usize, distance: &F,
+                        pending: &mut BinaryHeap<HeapElement<&KdNode<'a, T>>>,
+                        evaluated: &mut BinaryHeap<HeapElement<&'a T>>)
+        where F: Fn(&[f64], &[f64]) -> f64 {
+            let curr = pending.pop();
+
+            if curr.is_none() {
+                return;
+            }
+
+            let mut curr = &*curr.unwrap().element;
+
+            while !curr.is_leaf() {
+                let candidate;
+                if point[curr.split_dimension.unwrap()] > curr.split_value.unwrap() {
+                    candidate = unsafe {&*curr.left.unwrap()};
+                    curr = unsafe {&*curr.right.unwrap()};
+                } else {
+                    candidate = unsafe {&*curr.right.unwrap()};
+                    curr = unsafe {&*curr.left.unwrap()};
+                }
+                let candidate_to_space= util::distance_to_space(point, &*curr.min_bounds, &*curr.max_bounds, distance);
+                if evaluated.len() < num || candidate_to_space<= evaluated.peek().unwrap().distance {
+                    pending.push(HeapElement {
+                        distance: candidate_to_space* -1f64,
+                        element: candidate
+                    });
+                }
+            }
+
+            for i in 0..curr.size {
+                let p = curr.points.as_ref().unwrap()[i];
+                let d = curr.bucket.as_ref().unwrap()[i];
+                let p_to_point = distance(p, point);
+                if evaluated.len() < num {
+                    evaluated.push(HeapElement {
+                        distance: p_to_point,
+                        element: d
+                    });
+                } else if p_to_point < evaluated.peek().unwrap().distance {
+                    evaluated.pop();
+                    evaluated.push(HeapElement {
+                        distance: p_to_point,
+                        element: d
+                    });
+                }
+            }
+        }
+
+    pub fn add(&mut self, point: &'a [f64], data: &'a T) -> Result<(), ErrorKind> {
+        match self.check_point(point) {
+            Err(err) => return Err(err),
+            Ok(_) => {}
+        };
+        let mut curr = &mut *self;
+        while !curr.is_leaf() {
+            curr.extend(point);
+            curr.size += 1;
+            if point[curr.split_dimension.unwrap()] > curr.split_value.unwrap() {
+                curr = unsafe {&mut *curr.left.unwrap()};
+            } else {
+                curr = unsafe {&mut *curr.right.unwrap()};
+            }
+        }
+        curr.add_to_bucket(point, data);
+        Ok(())
+    }
+
+    fn add_to_bucket(&mut self, point: &'a [f64], data: &'a T) {
+        self.extend(point);
+        let mut points = self.points.take().unwrap();
+        let mut bucket = self.bucket.take().unwrap();
+        points.push(point);
+        bucket.push(data);
+        self.size += 1;
+        if self.size >= self.capacity {
+            self.split(points, bucket);
+        } else {
+            self.points = Some(points);
+            self.bucket = Some(bucket);
+        }
+    }
+
+    fn split(&mut self, points: Vec<&'a [f64]>, bucket: Vec<&'a T>) {
+        let mut max = 0f64;
+        for dim in 0..self.dimensions {
+            let diff = self.max_bounds[dim] - self.min_bounds[dim];
+            if !diff.is_nan() && diff > max {
+                max = diff;
+                self.split_dimension = Some(dim);
+            }
+        }
+        match self.split_dimension {
+            None => return,
+            Some(dim) => {
+                let min = self.min_bounds[dim];
+                let max = self.max_bounds[dim];
+                self.split_value = Some(min + (max - min) / 2f64);
+            }
+        };
+        let left: *mut KdNode<T> = &mut KdNode::<T>::new_with_capacity(self.dimensions, self.capacity);
+        let right: *mut KdNode<T> = &mut KdNode::<T>::new_with_capacity(self.dimensions, self.capacity);
+        for i in 0..points.len() {
+            if points[i][self.split_dimension.unwrap()] < self.split_value.unwrap() {
+                unsafe {
+                    (*left).add_to_bucket(points[i], bucket[i]);
+                }
+            } else {
+                unsafe {
+                    (*right).add_to_bucket(points[i], bucket[i]);
+                }
+            }
+        }
+        self.left = Some(left);
+        self.right = Some(right);
+    }
+
+    fn extend(&mut self, point: &[f64]) {
+        for dim in 0..self.dimensions {
+            if self.min_bounds[dim].is_nan() || self.min_bounds[dim] > point[dim] {
+                self.min_bounds[dim] = point[dim];
+            }
+            if self.max_bounds[dim].is_nan() || self.max_bounds[dim] < point[dim] {
+                self.max_bounds[dim] = point[dim];
+            }
+        }
+    }
+
+    fn is_leaf(&self) -> bool {
+        self.bucket.is_some() && self.points.is_some() &&
+            self.split_value.is_none() && self.split_dimension.is_none() &&
+            self.left.is_none() && self.right.is_none()
+    }
+
+    fn check_point(&self, point: &[f64]) -> Result<(), ErrorKind> {
+        if self.dimensions != point.len() {
+            return Err(ErrorKind::WrongDimension);
+        }
+        for n in point {
+            if !n.is_finite() {
+                return Err(ErrorKind::NonFiniteCoordinate);
+            }
+        }
+        Ok(())
+    }
+}
