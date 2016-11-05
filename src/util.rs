fb381e15889419d897572fc52aae4deedc75fce1
Oscar David Arbeláez <odarbelaeze@gmail.com>
2016-11-04 20:01:15 -0500

    Add some test to understand distance to space

--- src/util.rs
+++ src/util.rs
@@ -14,3 +14,66 @@ where F: Fn(&[f64], &[f64]) -> f64 {
     }
     distance(p1, &p2[..])
 }
+
+
+#[cfg(test)]
+mod tests {
+    use super::distance_to_space;
+    use super::super::distance::squared_euclidean;
+    use std::f64::{INFINITY, NEG_INFINITY};
+
+    #[test]
+    fn test_normal_distance_to_space() {
+        let dis = distance_to_space(
+            &[0.0, 0.0],
+            &[1.0, 1.0],
+            &[2.0, 2.0],
+            &squared_euclidean
+            );
+        assert_eq!(dis, 2.0);
+    }
+
+    #[test]
+    fn test_distance_outside_inf() {
+        let dis = distance_to_space(
+            &[0.0, 0.0],
+            &[1.0, 1.0],
+            &[INFINITY, INFINITY],
+            &squared_euclidean
+            );
+        assert_eq!(dis, 2.0);
+    }
+
+    #[test]
+    fn test_distance_inside_inf() {
+        let dis = distance_to_space(
+            &[2.0, 2.0],
+            &[NEG_INFINITY, NEG_INFINITY],
+            &[INFINITY, INFINITY],
+            &squared_euclidean
+            );
+        assert_eq!(dis, 0.0);
+    }
+
+    #[test]
+    fn test_distance_inside_normal() {
+        let dis = distance_to_space(
+            &[2.0, 2.0],
+            &[0.0, 0.0],
+            &[3.0, 3.0],
+            &squared_euclidean
+            );
+        assert_eq!(dis, 0.0);
+    }
+
+    #[test]
+    fn distance_to_half_space() {
+        let dis = distance_to_space(
+            &[-2.0, 0.0],
+            &[0.0, NEG_INFINITY],
+            &[INFINITY, INFINITY],
+            &squared_euclidean
+            );
+        assert_eq!(dis, 4.0);
+    }
+}
